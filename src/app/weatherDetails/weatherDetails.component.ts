59953c7d40b27730b2223de5e1e155f47d1478ce
Daniel Juárez <danieljuarezp@hotmail.com>
2023-05-19 18:52:18 -0600

    fix

--- src/app/weatherDetails/weatherDetails.component.ts
+++ src/app/weatherDetails/weatherDetails.component.ts
@@ -1,16 +1,20 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'weather-details',
-  templateUrl: './weatherDetails.component.html',
-  styleUrls: ['./weatherDetails.component.scss']
+  selector: "weather-details",
+  templateUrl: "./weatherDetails.component.html",
+  styleUrls: ["./weatherDetails.component.scss"],
 })
-
 export class WeatherDetails implements OnInit {
   @Input() weatherData: data[];
 
-  ngOnInit() {
+  selectedCity: data | null = null;
 
+  ngOnInit() {}
+
+  findCity(value) {
+    const city = value.target.value;
+    this.selectedCity = this.weatherData.find(q => q.name === city);
   }
 }
 
